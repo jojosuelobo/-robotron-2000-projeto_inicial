18098cfc6c2c7ba52c6bfb436708f8008ac91324
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-16 15:14:10 -0300

    Capitulo 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-const robotron = document.querySelector("#robotron") 
+const braco = document.querySelector("#braco") 
+const subtrair = document.querySelector("#subtrair") 
+const somar = document.querySelector("#somar") 
 
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+//somar.addEventListener("click", () => { manipulaDados("+") })
+//subtrair.addEventListener("click", () => { manipulaDados("-") })
+
+function manipulaDados(operacao){
+    if(operacao == "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const robotron = document.querySelector("#robotron") 
 
 function dizOiRobotron()
 {
